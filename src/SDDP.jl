ef82ddb74539b29c5cc2a86458b9a8f7af823707
Janik Königshofer <janik@koenigshofer-hh.de>
2024-08-28 09:53:24 +0200

    add settings global variable

--- src/SDDP.jl
+++ src/SDDP.jl
@@ -26,7 +26,7 @@ using RecipesBase
 
 export @stageobjective
 
-include("plugins/settings.jl")
+SETTINGS = JSON.Parser.parsefile("/Users/janik/Documents/Master/KIT/Masterarbeit/src/SDDP.jl/src/plugins/settings.json")
 
 logger = Logging.ConsoleLogger(stdout, SETTINGS["log_level"])
 Logging.global_logger(logger)
